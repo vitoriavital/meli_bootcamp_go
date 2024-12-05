17069d74eda0941aeb1b585e6d1f77f3f38dd8f0
mlvital_meli <maria.lvital@mercadolivre.com>
2024-12-05 19:56:16 -0300

    fix: remove unused struct

--- 11-go-web-post-method-meli/go-web/main.go
+++ 11-go-web-post-method-meli/go-web/main.go
@@ -30,16 +30,6 @@ type RequestBodyProduct struct {
 	Price		float64	`json:"price"`
 }
 
-type ProductData struct {
-	Id			int		`json:"id"`
-	Name		string	`json:"name"`
-	Quantity	int		`json:"quantity"`
-	CodeValue	string	`json:"code_value"`
-	IsPublished	bool	`json:"is_published"`
-	Expiration	string	`json:"expiration"`
-	Price		float64	`json:"price"`
-}
-
 type ResponseBodyProduct struct {
 	Message		string			`json:"message"`
 	Product	*Product 	`json:"product,omitempty"`
